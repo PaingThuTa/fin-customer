5a6917f51800216398abb72347149026f3f81314
PaingThuTa <paingthuta472@gmail.com>
2025-10-02 09:36:30 +0700

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/fin-customer',
+  experimental: {
+    instrumentationHook: true,
+  },
+};
+
+export default nextConfig;
